7ae7f803f9d2cb53a416a4e25d06a9a6901d50c7
bakp22 <101152896+bakp22@users.noreply.github.com>
2024-10-05 12:02:11 -0400

    Initial Commit

---
+++ WellnesWarriors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WellnesWarriors
+//
+//  Created by Beren Akpinar on 10/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WellnesWarriorsTests/WellnesWarriorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WellnesWarriorsTests.swift
+//  WellnesWarriorsTests
+//
+//  Created by Beren Akpinar on 10/5/24.
+//
+
+import Testing
+@testable import WellnesWarriors
+
+struct WellnesWarriorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
